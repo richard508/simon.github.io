ad0ad1937b8bf011353ed693431de55b454a14ec
Richard Almonte <richard.almonte@bridgesite.io>
2018-10-11 09:52:20 -0400

    mvp: create clickable panels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,51 @@
 $(document).ready(function(){
-  const start = $('.start')
-  const red = $('.red')
-  const green = $('.green')
-  const blue = $('.blue')
+	const start = $('.start')
+	const red = $('.red')
+	const green = $('.green')
+	const blue = $('.blue')
   const yellow = $('.yellow')
-  const borders = ['','border-top','border-bottom','border-left','border-right']
-  const simonSays = {
-    1: 'red',
-    2: 'blue',
-    3: 'green',
-    4: 'yellow'
-  }
-  const transactionalOrder = []
+  var begin = 'off'
+	const simonSays = {
+		1: 'red',
+		2: 'blue',
+		3: 'green',
+		4: 'yellow'
+	}
+	const transactionalOrder = []
 
-  function onStart () { 
-    var i = 1;
-    setTimeout(function () {
-      $('.'+simonSays[i]).css(borders[i], '150px solid '+simonSays[i])
-      i++;
-      if (i < 5) { 
-        onStart();
-      } else{
-        for(let i=1;i<=4;i++){
-          $('.'+simonSays[i]).removeAttr("style")
-        }
-      }
-    }, 200)
-  }
+	var i = 1;
+	function onStart () { 
+		setTimeout(function () {
+			$('.'+simonSays[i]).css('background', simonSays[i])
+			i++;
+			if (i < 5) { 
+				onStart();
+			} else{
+				setTimeout(function () {
+          for(let i=1;i<=4;i++){
+            $('.'+simonSays[i]).removeAttr("style")
+          }
+				}, 500)
+			}
+		}, 500)
+	}
 
-  start.click(function(){
-    $('.dot').css('background','red')
+	start.click(function(){
+		$('.dot').css('background','red')
     onStart()
+    begin = 'on'
+  })
+  // Math.floor(Math.random() * Math.floor(max))
+  red.click(function(){
+    console.log('red hit')
+  })
+  blue.click(function(){
+    console.log('blue hit')
+  })
+  green.click(function(){
+    console.log('hit green')
+  })
+  yellow.click(function(){
+    console.log('hit yellow')
   })
 })
